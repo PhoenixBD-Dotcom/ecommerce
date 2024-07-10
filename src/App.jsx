c364f49021283157a02680944b7798c36c69fd5e
Md.Sakibullah <sakibullah98@gmail.com>
2024-07-10 10:06:05 +0600

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-export default function App() {
-  return (
-    <div className="">App</div>
-  )
-}
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () =>{
+  return(
+    <div>
+      <Navbar/>
+    </div>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fddda9",
+        secondary: "#ed8900",
+      },
+      container:{
+        center: true,
+        padding:{
+          Default: "1rem",
+          sm: "1rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        }
+      }
+    },
   },
   plugins: [],
 }
